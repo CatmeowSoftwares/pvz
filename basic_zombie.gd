aaa9f57d447e76655bd9a846b943aeb52804a6c0
MysticSlime <jorenjacob098@gmail.com>
2026-05-31 22:27:50 +0800

    added death system

--- basic_zombie.gd
+++ basic_zombie.gd
@@ -1,7 +1,6 @@
 extends Zombie
 
 
-
 func _physics_process(delta: float) -> void:
 	if !eating: 
 		position.x -= 16 * delta
--- zombie.gd
+++ zombie.gd
@@ -1,9 +1,11 @@
 class_name Zombie extends Area2D
-
+signal death
 var health: int
 var eating: bool
 
 var plant_eating: Plant
+func _ready() -> void:
+	death.connect(_on_death)
 func _on_area_entered(area: Area2D) -> void:
 	plant_eating = area as Plant
 	eating = true
@@ -18,8 +20,11 @@ func eat():
 
 func normal_damage():
 	health -= 1
+	if health <= 0:
+		death.emit()
 
-
+func _on_death():
+	queue_free()
 func _on_eat_timer_timeout() -> void:
 	if plant_eating:
 		eat()
